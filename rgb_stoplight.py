eba1e1d26e8519ad7bbd138e5f2c1819806e8e39
Ainsley-M2025 <atmartinez2025@gmail.com>
2024-10-08 13:57:42 -0600

    Add files via upload

---
+++ rgb_stoplight.py
@@ -0,0 +1,17 @@
+import RPi.GPIO as GPIO
+import time
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(23, GPIO.OUT)
+print("LED on")
+GPIO.output(23, GPIO.HIGH)
+time.sleep(5)
+GPIO.setup(16, GPIO.OUT)
+print("LED on")
+GPIO.output(16, GPIO.HIGH)
+time.sleep(1)
+print("LED off")
+GPIO.output(23, GPIO.LOW)
+time.sleep(4)
+GPIO.output(16, GPIO.LOW)
+print("LED off")
